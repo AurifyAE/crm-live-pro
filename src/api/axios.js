7f58bff17df2528b2b9482705960202cc1fc65d9
ABDUL MAJID K <majidmaji3410@gmail.com>
2025-04-29 15:26:20 +0530

    udpate the axios url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8080/api/admin",
+  baseURL: "https://crm-live-pro-server.onrender.com/api/admin",
   headers: {
     'Content-Type': 'application/json',
     'x-secret-key': 'IfiuH/ko+rh/P3xwIfo0e8YTN1INFpkTU0knvGJjMO'
